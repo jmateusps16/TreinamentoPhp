10b53dbb2b8623c562b272c19fd8b8ac1d392774
Mateus Palmeira <jmateusps16@gmail.com>
2023-08-08 22:17:34 -0300

    iniciando com listas

---
+++ 02_arrays_strings_funcoes_e_web/01-listas.php
@@ -0,0 +1,8 @@
+<?php
+
+$idadeList = [21, 23, 19, 25, 30, 41, 18];
+$umaIdade = $idadeList[4];
+
+$filmes = array('Avengers: Endgame', 'Captain Marvel', 'Homem-Formiga e a Vespa');
+
+echo $umaIdade;
---
+++ 02_arrays_strings_funcoes_e_web/02-loop-lista.php
@@ -0,0 +1,7 @@
+<?php
+
+$filmes = array('Avengers: Endgame', 'Captain Marvel', 'Homem-Formiga e a Vespa');
+
+for ($i = 0; $i < count($filmes); $i++) {
+    echo $filmes[$i] . PHP_EOL;
+}
